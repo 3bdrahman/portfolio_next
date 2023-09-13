c8764b95182ff9129d41391e81987ee838381f17
Abdelrahman Youssef <abdelrahmany223@gmail.com>
2023-09-12 20:09:23 -0400

    add email functionality and footer

--- actions/sendEmail.tsx
+++ actions/sendEmail.tsx
@@ -1,6 +1,8 @@
 "use server";
 import { Resend } from "resend";
-import { validateString } from "@/lib/utils";
+import { validateString, getErrorMessage } from "@/lib/utils";
+import ContactFormEmail from "@/email/contact-form-email";
+import React from "react";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail =async (formData: FormData) =>{
@@ -16,17 +18,21 @@ export const sendEmail =async (formData: FormData) =>{
             error: "Invalid message"
         }
     }
-  
     try{
         await resend.emails.send({
             from: 'Contact Form <onboarding@resend.dev>',
             to: 'abdelrahmany223@gmail.com',
             subject: 'Message from contact form',
             reply_to: senderEmail as string,
-            text:message as string,
+            // text:message as string,
+            react: React.createElement(ContactFormEmail, {message: message as string, senderEmail:senderEmail as string}),
         })
-    }catch(error){
-            console.log(error)
+    }catch(error: unknown){
+            
+            return{
+                error: getErrorMessage(error)
+            }
+            
     }
     
 };
--- components/contact.tsx
+++ components/contact.tsx
@@ -1,13 +1,13 @@
 "use client"
 import React from 'react'
 import SectionHeading from './section-heading'
-import { FaPaperPlane } from 'react-icons/fa'
 import { motion } from 'framer-motion'
 import { useSectionInView } from '@/lib/hooks'
 import { sendEmail} from '@/actions/sendEmail'
+import SubmitButton from './submit-btn'
 export default function Contanct() {
     const{ref} = useSectionInView("Contact");
-   
+    
   return (
     
     <motion.section id='contact' ref={ref} className='scroll-mt-28 mb-20 sm:mb-28 w-[min(100%,38rem)] text-center' initial={{opacity:0}} whileInView={{opacity:1}} transition={{duration: 1}} viewport={{once: true}}>
@@ -18,9 +18,7 @@ export default function Contanct() {
         }} >
             <input name='senderEmail' placeholder='Your email' required maxLength={500} className='h-14 px-4 rounded-lg border border-black/10 ' type='email'/>
             <textarea name='message' placeholder='Your message' required maxLength={5000} className='h-52 my-3 rounded-lg border border-black/10 p-4'/>
-            <button className='group flex gap-2 h-[3rem] w-[8rem] bg-gray-900 rounded-full 
-            outline-none transition-all text-white items-center justify-center focus:scale-110 hover:scale-110 active:scale-105 hover:bg-gray-950' 
-            type='submit'>Submit <FaPaperPlane className='text-xs opacity-70 group-hover:translate-x-1 group-hover:-translate-y-2'/> </button>
+            <SubmitButton/>
         </form>
     </motion.section>
   )
